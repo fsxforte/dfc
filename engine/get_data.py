245768ef670aa7820868d1945c5cee4ead06b55a
Lewis <lewis@ipswich.doc.ic.ac.uk>
2019-12-09 20:26:21 +0000

    First approach to systematic volatility modelling

--- engine/get_data.py
+++ engine/get_data.py
@@ -4,26 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 from datetime import datetime
+import time
+
+time_now = int(time.time())
 
 def fetch_data():
 	'''
 	Fetch Grin/USDC price data from the Poloniex exchange, for the period 01 Nov until 21 Nov. 
 	'''
-
 	POLONIEX_PUBLIC_ENDPOINT = 'https://poloniex.com/public'
-
-	query_params = {'command':'returnChartData', 'currencyPair': 'USDC_GRIN', 'period': '300', 'start': '1572566400', 'end': '1574348631'}
-
+	query_params = {'command':'returnChartData', 'currencyPair': 'USDC_GRIN', 'period': '1800', 'start': '1546300800', 'end': str(time_now)}
 	data = requests.get(url = POLONIEX_PUBLIC_ENDPOINT, params = query_params)
-
 	data = data.json()
-
 	df = pd.DataFrame(data)
 	df['log_returns'] = np.log(df['close']) - np.log(df['close'].shift(1))
 	df['timestamp'] = df['date']
 	df['date'] = pd.to_datetime(df['date'], unit='s')
 	df = df.set_index('date')
-
 	return df
 
 def fetch_orderbook():
@@ -32,7 +29,7 @@ def fetch_orderbook():
 	'''
 	POLONIEX_PUBLIC_ENDPOINT = 'https://poloniex.com/public'
 
-	query_params = {'command':'returnTradeHistory', 'currencyPair': 'USDC_GRIN', 'depth': '1000', 'start': '1572566400', 'end': '1574348631'}
+	query_params = {'command':'returnTradeHistory', 'currencyPair': 'USDC_GRIN', 'depth': '1000', 'start': '1546300800', 'end': str(time_now)}
 
 	data = requests.get(url = POLONIEX_PUBLIC_ENDPOINT, params = query_params)
 
---
+++ scripts/modelling_returns.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from arch import arch_model
+from scipy import stats
+from sklearn.neighbors import KernelDensity
+import numpy as np
+
+
+from engine import get_data
+
+
+#0. Get the data and plot the log returns
+df = get_data.fetch_data()
+log_returns = df['log_returns']
+log_returns.plot()
+#Reveals that there is a significant outlier early on in the series --> restrict data to April 2019 onwards
+
+df_subset = df[dt.datetime(2019, 4, 1):]
+log_returns = df_subset['log_returns']
+log_returns.plot()
+#Plot superficially shows evidence of correlated residuals
+
+#1. Inspect ACF and PACF
+plot_acf(log_returns, lags = 100) # First 3 lags are significant
+plot_pacf(log_returns, lags = 100) # First 4 lags are significant
+
+#Test for Normality
+stats.jarque_bera(log_returns) #Reject normality
+
+#Fit standard GARCH model with constant mean
+am = arch_model(100*log_returns)
+res_normal = am.fit(update_freq=5)
+print(res.summary())
+fig = res.plot(annualize='D') # Evidence of considerable persistence
+
+# #Asymmetric GARCH (GJR GARCH)
+# am = arch_model(100*log_returns, p=1, o=1, q=1)
+# res = am.fit(update_freq=5, disp='off')
+# print(res.summary())
+# fig = res.plot(annualize='D') # Evidence of considerable persistence
+
+# #TARCH
+# am = arch_model(log_returns, p=1, o=1, q=1, power=1.0)
+# res = am.fit(update_freq=5)
+# print(res.summary())
+# fig = res.plot(annualize='D') # Evidence of considerable persistence
+# plt.show()
+
+#Standardize the residuals
+std_resid = res_normal.resid / res_normal.conditional_volatility
+unit_var_resid = res_normal.resid / res_normal.resid.std()
+df = pd.concat([std_resid, unit_var_resid], 1)
+df.columns = ['Std Resids', 'Unit Variance Resids']
+subplot = df.plot(kind='kde', xlim=(-4, 4))
+
+#Multiply the standardized returns by the current volatility estimate (scaling all returns by the current conditional volatility estimate)
+scaled_resids = res_normal.conditional_volatility[-1] * unit_var_resid
+
+#Convert to the right format for the Kernel Density estimation
+array = log_returns.to_numpy()
+array_returns = array.reshape(-1,1)
+
+#Fit the Epanechnikov kernel
+kde = KernelDensity(kernel = 'epanechnikov').fit(array_returns)
+X_plot = np.linspace(-1, 1, 1000)[:, np.newaxis]
+log_dens = kde.score_samples(X_plot)
+
+fig, ax = plt.subplots()
+ax.plot(X_plot[:, 0], np.exp(log_dens))
+plt.show()
